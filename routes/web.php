2c7c691b651c5a31658474097cb36afd14e4a937
Lobo33 <konta.faty.kf@gmail.com>
2022-07-30 01:39:21 +0000

    commit2

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::resource('/departements', 'DepartementsController');
 
-=======
 Route::resource('/utilisateurs', 'UtilisateursController');
->>>>>>> 8d6ad52413589653487e4950899d72bc5f5b002f
