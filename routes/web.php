f0661790bd1c3d48b1c06d985659ae348566613d
tounkaradiagui <tounkaradiagui@gmail.com>
2022-07-30 15:37:11 +0200

    second

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::get('secretaire',[App\Http\Controllers\SecretairesController::class, 'index']);
-=======
-Route::resource('departements', 'DepartementsController');
->>>>>>> 6ed55235889e09f5d209a0eceb8cd568b2db71fc
