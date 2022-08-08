c6269b964f9e8d516561dede60176d60b0127be1
Mariama DAOU <mariamadaou96@gmail.com>
2022-08-08 13:07:46 +0000

    commit devmaria1

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::post('secretaire',[App\Http\Controllers\SecretairesController::class, 'st
 Route::resource('/utilisateurs', 'UtilisateursController');
 Route::resource('/departements', 'DepartementsController');
 
+Route::resource('/courriers_entrants', 'CourriersEntrantsController');
+
 
 
 
