f202c7d34ee46e7cd373b9d23b8ad5be76f40db1
DOUMBIAAissata95 <aissadoumb56@gmail.com>
2022-08-08 13:45:15 +0000

    nouveau

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,6 @@ Route::post('secretaire',[App\Http\Controllers\SecretairesController::class, 'st
 Route::resource('/utilisateurs', 'UtilisateursController');
 Route::resource('/departements', 'DepartementsController');
 
-Route::resource('/courriers_entrants', 'CourriersEntrantsController');
 
 
 
