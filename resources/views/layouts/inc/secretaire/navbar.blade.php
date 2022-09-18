3cc1e97b735cbe73efc887d13ba7ec511efb8dd9
tounkaradiagui <tounkaradiagui@gmail.com>
2022-09-18 18:17:08 +0200

    secondOne

--- resources/views/layouts/inc/secretaire/navbar.blade.php
+++ resources/views/layouts/inc/secretaire/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex justify-content-center" style="background-color: #0E4C92">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
-          <a class="navbar-brand brand-logo" href="index.html">
+          <a class="navbar-brand brand-logo" href="#">
             <img src="{{asset('admin/images/bataki.png')}}" alt="E-Bataki" style=" width: 60px; height: 50px;" />
 
 
           
           </a>
-          <a class="navbar-brand brand-logo-mini" href="index.html">
+          <a class="navbar-brand brand-logo-mini" href="#">
             <!-- <img src="{{asset('admin/images/logogolden.png')}}" alt="logo"/> -->
           </a>
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::prefix('users')->group(function() {
 });
 
 Route::prefix('secretaire')->group(function() {
-    Route::get('dashboard', [App\Http\Controllers\secretaires\DashboardSecretairesController::class, 'index']);
+    Route::get('dashboard', [App\Http\Controllers\secretaires\DashboardController::class, 'index']);
     Route::get('secretaire',[App\Http\Controllers\Admin\SecretaireController::class, 'voircourrier'])->name('mescourriers');
     Route::get('envoyercourrierformulaire/{id}',[App\Http\Controllers\Admin\SecretaireController::class, 'voirform'])->name('sendformuser');
     Route::post('envoiuserformulaire',[App\Http\Controllers\Admin\SecretaireController::class, 'voircourrier'])->name('sendcourriertodestinataire');
